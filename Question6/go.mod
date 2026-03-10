54d7eb8b3e3019b99941674a74f294ce97abac81
Ehioje Henry Erabor <ehiojee@yahoo.com>
2026-03-10 17:12:14 +0000

    checking if array integers are permutative

---
+++ Question6/go.mod
@@ -0,0 +1,3 @@
+module question-six
+
+go 1.25.5
---
+++ Question6/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func IsPermutation(arr []int) int {
+	seen := make([]bool, len(arr)+1)
+	for _, num := range arr {
+		if num < 1 || num > len(arr) || seen[num] {
+			return 0
+		}
+	}
+
+	return 1
+}
+
+func main() {
+	arrOne, arrTwo := []int{5, 2, 3, 1, 4}, []int{2, 3, 5, 1}
+	fmt.Println("checking if the arrays/slice above are permutative")
+	fmt.Println("an output of 1 means it is permutative while an output of 0 means it's not")
+	fmt.Printf("array:: %#v :: %d\n", arrOne, IsPermutation(arrOne))
+	fmt.Printf("array:: %#v :: %d\n", arrOne, IsPermutation(arrTwo))
+}
